7953cd850114e7649d32acf885f3f22c89df128e
YOSMAR728 <yosmarr57@gmail.com>
2025-05-07 10:58:31 -0500

    mi segundo cargue, app flook

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+# Importamos la clase Flask desde el módulo flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hola mundo"
+if __name__ == "__main__":
+    app.run(debug=True)
